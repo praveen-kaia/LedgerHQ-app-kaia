4604aa2a4ed44a605c30a834e7ac3b93281e3fd7
keiff3r <keiffer@blooo.io>
2024-04-04 15:10:23 +0200

    test: write function perform_test_sign_tx_with_raw_tx - this function can be called with a raw_tx to perform a signing test

--- tests/test_sign_cmd.py
+++ tests/test_sign_cmd.py
@@ -1,15 +1,54 @@
-# TODO: Edit this test to match with the Klaytn application
+import pytest
 
-# import pytest
+from application_client.klaytn_transaction import Transaction
+from application_client.klaytn_command_sender import KlaytnCommandSender, Errors
+from application_client.klaytn_response_unpacker import unpack_get_public_key_response, unpack_sign_tx_response
+from ragger.error import ExceptionRAPDU
+from ragger.navigator import NavInsID
+from utils import ROOT_SCREENSHOT_PATH, check_signature_validity
 
-# from application_client.klaytn_transaction import Transaction
-# from application_client.klaytn_command_sender import KlaytnCommandSender, Errors
-# from application_client.klaytn_response_unpacker import unpack_get_public_key_response, unpack_sign_tx_response
-# from ragger.error import ExceptionRAPDU
-# from ragger.navigator import NavInsID
-# from utils import ROOT_SCREENSHOT_PATH, check_signature_validity
+# In theses tests we check the behavior of the device when asked to sign a transaction
 
-# # In this tests we check the behavior of the device when asked to sign a transaction
+
+
+# This function is a parametrized test that will test the signing of a transaction with a raw transaction in hex
+def perform_test_sign_tx_with_raw_tx(firmware, backend, navigator, test_name, raw_transaction_hex):
+    # Use the app interface instead of raw interface
+    client = KlaytnCommandSender(backend)
+    # The path used for this entire test
+    path: str = "m/44'/8217'/0'/0/0"
+
+    # First we need to get the public key of the device in order to build the transaction
+    rapdu = client.get_public_key(path=path)
+    _, public_key, _, _ = unpack_get_public_key_response(rapdu.data)
+
+    # Convert the raw transaction hex to bytes
+    raw_transaction_bytes = bytes.fromhex(raw_transaction_hex)
+
+    # Send the sign device instruction.
+    # As it requires on-screen validation, the function is asynchronous.
+    # It will yield the result when the navigation is done
+    with client.sign_tx(path=path, transaction=raw_transaction_bytes):
+        # Validate the on-screen request by performing the navigation appropriate for this device
+        if firmware.device.startswith("nano"):
+            navigator.navigate_until_text_and_compare(NavInsID.RIGHT_CLICK,
+                                                      [NavInsID.BOTH_CLICK],
+                                                      "Approve",
+                                                      ROOT_SCREENSHOT_PATH,
+                                                      test_name)
+        else:
+            navigator.navigate_until_text_and_compare(NavInsID.USE_CASE_REVIEW_TAP,
+                                                      [NavInsID.USE_CASE_REVIEW_CONFIRM,
+                                                       NavInsID.USE_CASE_STATUS_DISMISS],
+                                                      "Hold to sign",
+                                                      ROOT_SCREENSHOT_PATH,
+                                                      test_name)
+
+    # The device as yielded the result, parse it and ensure that the signature is correct
+    response = client.get_async_response().data
+    _, der_sig, _ = unpack_sign_tx_response(response)
+    assert check_signature_validity(public_key, der_sig, raw_transaction_bytes)
+
 
 
 # # In this test se send to the device a transaction to sign and validate it on screen
