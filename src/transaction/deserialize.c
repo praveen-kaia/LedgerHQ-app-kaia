36c795a7238d049ee398a3cf4c84ede54fe58166
keiff3r <keiffer@blooo.io>
2024-04-11 18:22:40 +0200

    feat: Add RLP parsing functionality to deserialize

--- src/transaction/deserialize.c
+++ src/transaction/deserialize.c
@@ -34,39 +34,147 @@ parser_status_e transaction_deserialize(buffer_t *buf, transaction_t *tx) {
     if (buf->size > MAX_TX_LEN) {
         return WRONG_LENGTH_ERROR;
     }
-
-    // nonce
-    if (!buffer_read_u64(buf, &tx->nonce, BE)) {
-        return NONCE_PARSING_ERROR;
+    parser_context_t parsing_ctx = {
+        .txType = 0,
+        .currentField = RLP_NONE,
+        .processingField = false,
+    };
+    for(;;){
+        if(PARSING_IS_DONE(parsing_ctx)){
+            return PARSING_OK;
+        }
+        if ((parsing_ctx.txType == LEGACY && parsing_ctx.currentField == LEGACY_RLP_CHAIN_ID) &&
+            (buf->offset == buf->size)) {
+            tx->chainID.length = 0;
+            return PARSING_OK;
+        }
+        if (buf->offset == buf->size) {
+            PRINTF("Command length done\n");
+            return PARSING_PROCESSING;
+        }
+        if (!parsing_ctx.processingField) {
+            parser_status_e status = parseRLP(&parsing_ctx);
+            if (status != PARSING_CONTINUE) {
+                return status;
+            }
+        }
     }
 
-    tx->to = (uint8_t *) (buf->ptr + buf->offset);
+    // // nonce
+    // if (!buffer_read_u64(buf, &tx->nonce, BE)) {
+    //     return NONCE_PARSING_ERROR;
+    // }
+    // tx->to = (uint8_t *) (buf->ptr + buf->offset);
+    // // TO address
+    // if (!buffer_seek_cur(buf, ADDRESS_LEN)) {
+    //     return TO_PARSING_ERROR;
+    // }
+    // // amount value
+    // if (!buffer_read_u64(buf, &tx->value, BE)) {
+    //     return VALUE_PARSING_ERROR;
+    // }
+    // // length of memo
+    // if (!buffer_read_varint(buf, &tx->memo_len) && tx->memo_len > MAX_MEMO_LEN) {
+    //     return MEMO_LENGTH_ERROR;
+    // }
+    // // memo
+    // tx->memo = (uint8_t *) (buf->ptr + buf->offset);
+    // if (!buffer_seek_cur(buf, tx->memo_len)) {
+    //     return MEMO_PARSING_ERROR;
+    // }
+    // if (!transaction_utils_check_encoding(tx->memo, tx->memo_len)) {
+    //     return MEMO_ENCODING_ERROR;
+    // }
 
-    // TO address
-    if (!buffer_seek_cur(buf, ADDRESS_LEN)) {
-        return TO_PARSING_ERROR;
-    }
-
-    // amount value
-    if (!buffer_read_u64(buf, &tx->value, BE)) {
-        return VALUE_PARSING_ERROR;
-    }
-
-    // length of memo
-    if (!buffer_read_varint(buf, &tx->memo_len) && tx->memo_len > MAX_MEMO_LEN) {
-        return MEMO_LENGTH_ERROR;
-    }
-
-    // memo
-    tx->memo = (uint8_t *) (buf->ptr + buf->offset);
-
-    if (!buffer_seek_cur(buf, tx->memo_len)) {
-        return MEMO_PARSING_ERROR;
-    }
-
-    if (!transaction_utils_check_encoding(tx->memo, tx->memo_len)) {
-        return MEMO_ENCODING_ERROR;
-    }
 
     return (buf->offset == buf->size) ? PARSING_OK : WRONG_LENGTH_ERROR;
+}
+uint8_t readTxByte(parser_context_t *parser_ctx) {
+    uint8_t data;
+    if (parser_ctx->commandLength < 1) {
+        PRINTF("readTxByte Underflow\n");
+        CX_THROW(0x9999);
+    }
+    data = *parser_ctx->workBuffer;
+    parser_ctx->workBuffer++;
+    parser_ctx->commandLength--;
+    if (parser_ctx->processingField) {
+        parser_ctx->currentFieldPos++;
+    }
+    return data;
+}
+
+bool rlpCanDecode(uint8_t *buffer, uint32_t bufferLength, bool *valid) {
+    if (*buffer <= 0x7f) {
+    } else if (*buffer <= 0xb7) {
+    } else if (*buffer <= 0xbf) {
+        if (bufferLength < (1 + (*buffer - 0xb7))) {
+            return false;
+        }
+        if (*buffer > 0xbb) {
+            *valid = false;  // arbitrary 32 bits length limitation
+            return true;
+        }
+    } else if (*buffer <= 0xf7) {
+    } else {
+        if (bufferLength < (1 + (*buffer - 0xf7))) {
+            return false;
+        }
+        if (*buffer > 0xfb) {
+            *valid = false;  // arbitrary 32 bits length limitation
+            return true;
+        }
+    }
+    *valid = true;
+    return true;
+}
+
+static parser_status_e parseRLP(parser_context_t *parser_ctx) {
+    bool canDecode = false;
+    uint32_t offset;
+    while (parser_ctx->commandLength != 0) {
+        bool valid;
+        // Feed the RLP buffer until the length can be decoded
+        parser_ctx->rlpBuffer[parser_ctx->rlpBufferPos++] = readTxByte(parser_ctx);
+        if (rlpCanDecode(parser_ctx->rlpBuffer, parser_ctx->rlpBufferPos, &valid)) {
+            // Can decode now, if valid
+            if (!valid) {
+                PRINTF("RLP pre-decode error\n");
+                return PARSING_ERROR;
+            }
+            canDecode = true;
+            break;
+        }
+        // Cannot decode yet
+        // Sanity check
+        if (parser_ctx->rlpBufferPos == sizeof(parser_ctx->rlpBuffer)) {
+            PRINTF("RLP pre-decode logic error\n");
+            return PARSING_ERROR;
+        }
+    }
+    if (!canDecode) {
+        PRINTF("Can't decode\n");
+        return PARSING_PROCESSING;
+    }
+    // Ready to process this field
+    if (!rlpDecodeLength(parser_ctx->rlpBuffer,
+                         &parser_ctx->currentFieldLength,
+                         &offset,
+                         &parser_ctx->currentFieldIsList)) {
+        PRINTF("RLP decode error\n");
+        return PARSING_ERROR;
+    }
+    // Ready to process this field
+    if (offset == 0) {
+        // Hack for single byte, self encoded
+        parser_ctx->workBuffer--;
+        parser_ctx->commandLength++;
+        parser_ctx->fieldSingleByte = true;
+    } else {
+        parser_ctx->fieldSingleByte = false;
+    }
+    parser_ctx->currentFieldPos = 0;
+    parser_ctx->rlpBufferPos = 0;
+    parser_ctx->processingField = true;
+    return PARSING_CONTINUE;
 }
--- src/transaction/deserialize.h
+++ src/transaction/deserialize.h
@@ -4,6 +4,55 @@
 
 #include "types.h"
 
+#define RLP_NONE 0
+typedef enum {
+    LEGACY_RLP_NONE = RLP_NONE,
+    LEGACY_RLP_CONTENT,
+    LEGACY_RLP_TYPE,
+    LEGACY_RLP_NONCE,
+    LEGACY_RLP_GASPRICE,
+    LEGACY_RLP_STARTGAS,
+    LEGACY_RLP_TO,
+    LEGACY_RLP_VALUE,
+    LEGACY_RLP_DATA,
+    LEGACY_RLP_CHAIN_ID,
+    LEGACY_RLP_ZERO1,
+    LEGACY_RLP_ZERO2,
+    LEGACY_RLP_DONE
+} rlpLegacyTxField_e;
+
+typedef enum {
+    CANCEL_RLP_NONE = RLP_NONE,
+    CANCEL_RLP_CONTENT,
+    CANCEL_RLP_TYPE,
+    CANCEL_RLP_NONCE,
+    CANCEL_RLP_GASPRICE,
+    CANCEL_RLP_GASLIMIT,
+    CANCEL_RLP_FROM,
+    CANCEL_RLP_RATIO,
+    CANCEL_RLP_CHAIN_ID,
+    CANCEL_RLP_ZERO1,
+    CANCEL_RLP_ZERO2,
+    CANCEL_RLP_DONE
+} rlpCancelTxField_e;
+
+typedef struct {
+    uint8_t txType;  /// transaction type
+    uint8_t currentField;  /// current field being parsed
+    uint32_t currentFieldPos; /// position in the current field
+    uint32_t currentFieldLength; /// length of the current field
+    bool currentFieldIsList; /// flag to indicate if the current field is a list
+    uint8_t processingField;  /// flag to indicate if a field is being processed
+    bool fieldSingleByte;  /// flag to indicate if the field is a single byte
+    uint8_t commandLength;  /// length of the command
+    uint8_t rlpBuffer[5];  /// buffer to store RLP data
+    uint32_t rlpBufferPos;  /// position in the RLP buffer
+    const uint8_t *workBuffer; /// pointer to the buffer being parsed
+
+
+
+} parser_context_t;
+
 /**
  * Deserialize raw transaction in structure.
  *
@@ -16,3 +65,35 @@
  *
  */
 parser_status_e transaction_deserialize(buffer_t *buf, transaction_t *tx);
+
+/**
+ * Parse RLP fields.
+ *
+ * @param[in, out] parsing_ctx
+ *   Pointer to parsing context.
+ *
+ * @return PARSING_OK if success, error status otherwise.
+ *
+ */
+static parser_status_e parseRLP(parser_context_t *parsing_ctx);
+
+/**
+ * @def PARSING_IS_DONE(parsing_ctx)
+ * @brief Macro to check if parsing is done.
+ *
+ * This macro checks if parsing is complete by evaluating the current parsing context and transaction type.
+ * It returns true if parsing is done, and false otherwise.
+ *
+ * @param parsing_ctx The parsing context.
+ * @return True if parsing is done, false otherwise.
+ */
+#define PARSING_IS_DONE(parsing_ctx)                                                                     \
+        ((parsing_ctx.txType == LEGACY && parsing_ctx.currentField == LEGACY_RLP_DONE)||                  \
+        ((parsing_ctx.txType == CANCEL || parsing_ctx.txType == FEE_DELEGATED_CANCEL ||                 \
+            parsing_ctx.txType == PARTIAL_FEE_DELEGATED_CANCEL) &&                                           \
+            parsing_ctx.currentField == CANCEL_RLP_DONE))
+
+
+uint8_t readTxByte(parser_context_t *parser_ctx);
+
+bool rlpCanDecode(uint8_t *buffer, uint32_t bufferLength, bool *valid);
