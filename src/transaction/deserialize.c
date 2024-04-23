f309c4a506bcdc1b8369a5fc066fd5c59b41ee73
keiff3r <keiffer@blooo.io>
2024-04-23 16:06:45 +0200

    fix: add missing include in deserialize.c

--- src/transaction/deserialize.c
+++ src/transaction/deserialize.c
@@ -15,7 +15,7 @@
  *  limitations under the License.
  *****************************************************************************/
 #include <stdio.h>  // printf
-
+#include <string.h> // memmove
 #include "buffer.h"
 
 #include "deserialize.h"
