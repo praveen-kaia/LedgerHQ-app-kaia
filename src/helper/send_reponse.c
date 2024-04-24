b7cf11103cfaa7b738de8131ab7c10b81ad47dfb
keiff3r <keiffer@blooo.io>
2024-04-24 17:47:47 +0200

    feat: make get public key return a checksummed address

--- src/helper/send_reponse.c
+++ src/helper/send_reponse.c
@@ -21,6 +21,7 @@
 #include <stdio.h>  // PRINTF
 
 #include "buffer.h"
+#include "cx.h"
 
 #include "send_response.h"
 #include "../constants.h"
@@ -30,19 +31,70 @@
 
 #include "../transaction/utils.h"
 
-#define ADDRESS_IN_ASCII_HEX_LEN (ADDRESS_LEN * 2)
 
-// Converts each byte from the input to its ASCII hexadecimal representation in the output
-void hex_to_ascii_hex(const uint8_t *input_hex, uint8_t *output_hex, size_t input_len) {
-    for (size_t i = 0; i < input_len; i++) {
-        // High nibble to ASCII hexadecimal, ensuring upper case
-        int high = (input_hex[i] >> 4) & 0x0F;
-        output_hex[2 * i] = high < 10 ? '0' + high : 'A' + (high - 10);
+static const char HEXDIGITS[] = "0123456789abcdef";
 
-        // Low nibble to ASCII hexadecimal, ensuring upper case
-        int low = input_hex[i] & 0x0F;
-        output_hex[2 * i + 1] = low < 10 ? '0' + low : 'A' + (low - 10);
+void getEthAddressStringFromBinary(uint8_t *address, char *out, cx_sha3_t *sha3Context) {
+    // save some precious stack space
+    union locals_union {
+        uint8_t hashChecksum[HASH_LENGTH];
+        uint8_t tmp[51];
+    } locals_union;
+
+    uint8_t i;
+    uint32_t offset = 0;
+
+    for (i = 0; i < 20; i++) {
+        uint8_t digit = address[i];
+        locals_union.tmp[offset + 2 * i] = HEXDIGITS[(digit >> 4) & 0x0f];
+        locals_union.tmp[offset + 2 * i + 1] = HEXDIGITS[digit & 0x0f];
     }
+
+    CX_THROW(cx_keccak_init_no_throw(sha3Context, 256));
+
+    CX_THROW(cx_hash_no_throw((cx_hash_t *) sha3Context,
+                              CX_LAST,
+                              locals_union.tmp,
+                              offset + 40,
+                              locals_union.hashChecksum,
+                              32));
+
+    for (i = 0; i < 40; i++) {
+        uint8_t digit = address[i / 2];
+        if ((i % 2) == 0) {
+            digit = (digit >> 4) & 0x0f;
+        } else {
+            digit = digit & 0x0f;
+        }
+        if (digit < 10) {
+            out[i] = HEXDIGITS[digit];
+        } else {
+            int v = (locals_union.hashChecksum[i / 2] >> (4 * (1 - i % 2))) & 0x0f;
+            if (v >= 8) {
+                out[i] = HEXDIGITS[digit] - 'a' + 'A';
+            } else {
+                out[i] = HEXDIGITS[digit];
+            }
+        }
+    }
+    out[40] = '\0';
+}
+
+void getEthAddressStringFromKey(uint8_t *publicKey,
+                                char *out,
+                                cx_sha3_t *sha3Context) {
+    uint8_t hashAddress[HASH_LENGTH];
+
+    CX_THROW(cx_keccak_init_no_throw(sha3Context, 256));
+
+    CX_THROW(cx_hash_no_throw((cx_hash_t *) sha3Context,
+                              CX_LAST,
+                              publicKey + 1,
+                              64,
+                              hashAddress,
+                              32));
+
+    getEthAddressStringFromBinary(hashAddress + 12, out, sha3Context);
 }
 
 int helper_send_response_pubkey() {
@@ -54,13 +106,8 @@ int helper_send_response_pubkey() {
     offset += PUBKEY_LEN;
 
     resp[offset++] = ADDRESS_IN_ASCII_HEX_LEN;
-    uint8_t address[ADDRESS_LEN] = {0};
-    if (!address_from_pubkey(G_context.pk_info.raw_public_key, address, ADDRESS_LEN)) {
-        return io_send_sw(SW_ADDRESS_FAIL);
-    }
-    PRINTF("Address: %.*H\n", ADDRESS_LEN, address);
-    hex_to_ascii_hex(address, resp + offset, ADDRESS_LEN);
-    PRINTF("Address in ASCII hex: %.*s\n", ADDRESS_IN_ASCII_HEX_LEN, resp + offset);
+    cx_sha3_t sha3;
+    getEthAddressStringFromKey(G_context.pk_info.raw_public_key, (char *) resp + offset, &sha3);    
     offset += ADDRESS_IN_ASCII_HEX_LEN;
 
     resp[offset++] = CHAINCODE_LEN;
